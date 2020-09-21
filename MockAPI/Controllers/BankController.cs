c6575cb1ea8cbdbe807c2c3a7537fe2e95e62705
varghesechandy <VargheseAPIDemo>
2020-09-21 16:12:24 +0100

    Add project files.

---
+++ MockAPI/Controllers/BankController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MockAPI.Model;
+
+namespace MockAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankController : ControllerBase
+    {
+        [HttpPost]
+        public IEnumerable<string> Get([FromBody] PaymentData paymentData)
+        {
+            var statusCode = paymentData.Name.ToLower().Contains("fail") ? "20120" : paymentData.Name.ToLower().Contains("block") ? "40101" : paymentData.Name.ToLower().Contains("nofund") ? "200P9" : "10000";
+            var statusMessage = paymentData.Name.ToLower().Contains("fail") ? "Invalid Customer/User! Your payment request was unsuccessful." :
+                                paymentData.Name.ToLower().Contains("block") ? "The request was blocked by your risk settings! Your payment request was unsuccessful." :
+                                paymentData.Name.ToLower().Contains("nofund") ? "Limit exceeded! Enter a lesser value. Your payment request was unsuccessful." :
+                                "Your payment request was successful.";
+
+            return new string[] { Guid.NewGuid().ToString(), statusCode, statusMessage };
+        }
+    }
+}
---
+++ MockAPI/Model/PaymentData.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MockAPI.Model
+{
+    public class PaymentData
+    {
+        [Required]
+        public string CardNumber { get; set; }
+
+        public string Name { get; set; }
+
+        [Required]
+        public string Expiry { get; set; }
+
+        [Required]
+        public string Amount { get; set; }
+
+        [Required]
+        public string Currency { get; set; }
+
+        [Required]
+        public string Security { get; set; }
+    }
+
+}
